9aa58274859756dbfba8fbd5874b50a7d45e6e43
Daisy <MisterDeen1694@gmail.com>
2023-12-24 19:52:11 +0100

    :tada: Finished Course

--- app/src/main/java/me/daisydeen/todolist/MainActivity.kt
+++ app/src/main/java/me/daisydeen/todolist/MainActivity.kt
@@ -1,43 +1,38 @@
 package me.daisydeen.todolist
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import me.daisydeen.todolist.ui.theme.ToDoListTheme
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var todoAdaptor: TodoAdaptor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ToDoListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+        todoAdaptor = TodoAdaptor(mutableListOf())
+
+        val rvTodoItems: RecyclerView = findViewById(R.id.rvTodoItems)
+        rvTodoItems.adapter = todoAdaptor
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        val btnAddTodo: Button = findViewById(R.id.btnAddTodo)
+        val etTodoTitle: EditText = findViewById(R.id.etTodoTitle)
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdaptor.addTodo(todo)
+                etTodoTitle.text.clear()
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDoListTheme {
-        Greeting("Android")
+        val btnDeleteDoneTodos: Button = findViewById(R.id.btnDeleteTodo)
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdaptor.deleteDoneTodos()
+        }
     }
 }
